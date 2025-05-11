1559f73aecdae5c3a3b47553c9bd09c12fa96d26
tiye <jiyinyiyong@gmail.com>
2025-05-11 13:22:31 +0800

    Initial Commit

---
+++ bilibili-viewer/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  bilibili-viewer
+//
+//  Created by chen on 2025/5/11.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ bilibili-viewer/bilibili_viewerApp.swift
@@ -0,0 +1,33 @@
+//
+//  bilibili_viewerApp.swift
+//  bilibili-viewer
+//
+//  Created by chen on 2025/5/11.
+//
+
+import SwiftUI
+
+@main
+struct bilibili_viewerApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
